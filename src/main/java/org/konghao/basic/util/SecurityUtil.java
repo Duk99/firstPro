81d0f2ad0e80bb35537b1e91be58d0a5de9a2dfa
dukai <dukai1@yunzongnet.com>
2018-03-05 15:28:44 +0800

    3213123123

--- src/main/java/org/konghao/basic/util/SecurityUtil.java
+++ src/main/java/org/konghao/basic/util/SecurityUtil.java
@@ -5,6 +5,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SecurityUtil {
+	//111
 	public static String md5(String password) throws NoSuchAlgorithmException {
 		MessageDigest md = MessageDigest.getInstance("MD5");
 		md.update(password.getBytes());
